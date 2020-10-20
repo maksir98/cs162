33abaa59ca2627a8990f4a94a0910235a1c4b168
Kevin Svetlitski <kevin_svetlitski@berkeley.edu>
2020-10-19 23:18:15 -0500

    Add HW4 starter code

---
+++ hw4/mm_alloc.h
@@ -0,0 +1,18 @@
+/*
+ * mm_alloc.h
+ *
+ * Exports a clone of the interface documented in "man 3 malloc".
+ */
+
+#pragma once
+
+#ifndef _malloc_H_
+#define _malloc_H_
+
+#include <stdlib.h>
+
+void* mm_malloc(size_t size);
+void* mm_realloc(void* ptr, size_t size);
+void mm_free(void* ptr);
+
+#endif
